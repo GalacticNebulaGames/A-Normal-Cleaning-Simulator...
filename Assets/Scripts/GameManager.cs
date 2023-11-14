abfe2954c3c243008fca31af5faf418b616b5ec7
Galactic Nebula Games <gregorybrummel@gmail.com>
2023-11-14 17:40:33 +1100

    Alpha v.2.2
    In this update we have added:
    -night 2 and the story!
    -The has been updated to: Cleaning Simulator: The Hallway Dweller
    -Main menu UI
    -Endgame
    -Prefab workflow
    -And other bug fixes
    
    In the next update we will be adding all five nights and a little surprise ;)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,27 +10,71 @@ public GameObject[] nights;
 int currentNight=0;
 GameObject currentInstance;
 public GameObject menuCamera;
+public Text text;
+public Text button;
+public Font titleFont;
+public Font inGameFont;
     // Start is called before the first frame update
     void Start()
     {
-        ui.SetActive (true);
+      FirstNightUI();
 
     }
 
+    void FirstNightUI(){
+        ui.SetActive (true);
+        text.font = titleFont;
+        button.font = titleFont;
+        text.text = "Cleaning Simulator: The Hallway Dweller";
+        button.text = "New Game";
 
 
+    }
+
+    void ContinueNightUI(){
+        ui.SetActive (true);
+        text.font = inGameFont;
+        button.font = inGameFont;
+        text.text = "You Escaped!!!";
+        button.text = "Next Night";
 
 
+    }
 
-    public void HandleNightBegin(){
-    menuCamera.SetActive(false);
+ void FinalNightUI(){
+        ui.SetActive (true);
+        text.font = titleFont;
+        button.font = titleFont;
+        text.text = "The janintor finnishes his first week, never to be seen again. THE END";
+        button.text = "Back To Menu";
+
+
+    }
+    public void HandleButtonPress(){
+    
+        if (currentNight == nights.Length){
+        FirstNightUI();
+        currentNight = 0;
+
+
+        } else {
+menuCamera.SetActive(false);
     currentInstance = GameObject.Instantiate (nights[currentNight]);
      ui.SetActive (false);
     Cursor.lockState = CursorLockMode.Locked;
 
+        }
+        
+
     }
       public void HandleNightEnd(){
-        ui.SetActive(true);
+        currentNight++;
+        if(currentNight == nights.Length){
+        FinalNightUI();
+         }else{
+          ContinueNightUI();
+         }
+          
        menuCamera.SetActive(true);
        GameObject.Destroy(currentInstance);
        Cursor.lockState = CursorLockMode.None;
